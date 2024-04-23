095762e34443856e8799c9bfda60ad2eac6a39ff
Kamil Pinas <kamil.pinas.pl@gmail.com>
2024-04-23 21:21:22 +0200

    bug fix

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -18,9 +18,9 @@ class Simulation():
     def get_lidar_data(self) -> np.ndarray:
         lst = np.zeros((self.num_measurements, 2))
         lst[:, 0] = np.linspace(0, np.pi*2, self.num_measurements, endpoint=False)
-        position = np.array([self.object.x_pos, self.object.y_pos])
+        position = np.array([self.object.real_x_pos, self.object.real_y_pos])
         for i in range(self.num_measurements):
-            angle = self.object.angle + lst[i, 0]
+            angle = self.object.real_angle + lst[i, 0]
             v = np.array([np.cos(angle), np.sin(angle)])
             v_sum = v + position
             num_iter = 1
