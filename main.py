93f5ef8f3044fdc78627218574fcaaf74e5a4377
Kamil Pinas <kamil.pinas.pl@gmail.com>
2024-04-23 21:03:33 +0200

    move noise

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from keyboard import is_pressed
 from simulation import Object, Simulation, show_lidar_data
 
 
-object = Object(100, 100, size=8)
-sim = Simulation('maps/map.png', num_measurements=500, std=0, object=object)
+object = Object(100, 100, move_noise_std=0.1, rotation_noise_std=0.04, offset = 0.01, size=8)
+sim = Simulation('maps/map.png', num_measurements=500, lidar_std=0,object=object)
 window_name = "Lidar Simulation"
 
 
@@ -14,20 +14,13 @@ while not is_pressed('q'):
     cv2.imshow(window_name, sim.get_img())
 
     if is_pressed('w'):
-        sim.move(0, 5, 0)
+        object.move(5)
     if is_pressed("s"):
-        sim.move(0, -5, 0)
+        object.move(-5)
     if is_pressed('a'):
-        sim.move(-5, 0, 0)
+        object.rotate(0.1)
     if is_pressed('d'):
-        sim.move(5, 0, 0)
-
-    #rotate left
-    if is_pressed('r'):
-        sim.move(0, 0, 0.1)
-    #rotate right
-    if is_pressed('t'):
-        sim.move(0, 0, -0.1)
+        object.rotate(-0.1)
 
     if is_pressed('g'):
         lst = sim.get_lidar_data()
--- simulation/object.py
+++ simulation/object.py
@@ -2,15 +2,24 @@ import numpy as np
 
 
 class Object():
-    def __init__(self, x_pos:float, y_pos:float, angle:float=np.pi/2, color1:int=200 , color2:int=50, size:int=10):
+    def __init__(self, x_pos:float, y_pos:float, move_noise_std:float = 0, rotation_noise_std:float=0, offset:float=0, angle:float=np.pi/2, color1:int=200 , color2:int=50, size:int=10):
         if color1 == 0 or color2 == 0:
             raise Exception("color value cannot be 0 ")
         self.size = size
         self.color1 = color1
         self.color2 = color2
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.angle = angle         
+        self.rotation_noise_std = rotation_noise_std
+        self.move_noise_std = move_noise_std
+        self.offset = offset
+
+        self.real_x_pos = x_pos
+        self.real_y_pos = y_pos
+        self.real_angle = angle
+
+        # theoretical position base on input of move and roate function if noise = 0, x_pos = real_x_pos ...
+        self.x = x_pos
+        self.y = y_pos
+        self.angle = angle
 
         self.object_matrix = np.zeros((2, (2*(size+1))**2))
         iter = 0
@@ -20,8 +29,41 @@ class Object():
                 iter += 1
 
 
+    def move_raw(self, delta_x: float, delta_y: float, delta_angle:float):
+        self.real_x_pos += delta_x
+        self.real_y_pos += delta_y
+        self.real_angle += delta_angle
+
+
+    def rotate(self, angle: float, random_noise: bool=True):
+        if self.rotation_noise_std != 0 and random_noise:
+            self.real_angle += angle + np.random.randn(1)[0] * self.rotation_noise_std + self.offset
+            self.real_y_pos += np.random.rand(1)[0]*self.move_noise_std * 0.1
+            self.real_x_pos += np.random.rand(1)[0]*self.move_noise_std * 0.1
+        else:
+            self.real_angle += angle 
+        
+        self.angle += angle
+
+
+    def move(self, y: float, random_noise: bool=True):
+        if self.rotation_noise_std != 0 and random_noise:
+            noise = self.rotation_noise_std * np.random.randn(1)[0] + self.offset
+            self.real_y_pos += np.sin(self.real_angle + noise) * y + np.random.rand(1)[0]*self.move_noise_std
+            self.real_x_pos += np.cos(self.real_angle + noise) * y + np.random.rand(1)[0]*self.move_noise_std
+            self.x += np.sin(self.real_angle) * y
+            self.y += np.cos(self.real_angle) * y
+        else:
+            dx = np.cos(self.real_angle) * y
+            dy = np.sin(self.real_angle) * y
+            self.real_y_pos += dy
+            self.real_x_pos += dx
+            self.x += dx
+            self.y += dy
+
+
     def rotate_object(self) -> np.ndarray:
-        rotation_matrix = np.array([[np.cos(self.angle), -np.sin(self.angle)], [np.sin(self.angle), np.cos(self.angle)]])
+        rotation_matrix = np.array([[np.cos(self.real_angle), -np.sin(self.real_angle)], [np.sin(self.real_angle), np.cos(self.real_angle)]])
         return rotation_matrix.dot(self.object_matrix)
          
 
@@ -30,7 +72,7 @@ class Object():
         rotated_obj = self.rotate_object()
         different_color_counter = self.object_matrix.size//4
         for i in range(self.object_matrix.shape[1]):
-            x, y = round(self.x_pos+rotated_obj[0, i]), round(self.y_pos+rotated_obj[1, i])
+            x, y = round(self.real_x_pos+rotated_obj[0, i]), round(self.real_y_pos+rotated_obj[1, i])
             if 0 <= x < img.shape[1] and 0 < y < img.shape[0]:
                 if different_color_counter:
                     img[img.shape[0]-y, x] = self.color2 
