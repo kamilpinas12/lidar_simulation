70d420d5bf4e3b02303b35cbf436f90264e615cf
Filip Polatynski <fpolatynski@gmail.com>
2024-04-24 13:07:52 +0200

    Window and control

--- main.py
+++ main.py
@@ -1,46 +1,11 @@
-import numpy as np
-import cv2
-from keyboard import is_pressed
 
-from simulation import Object, Simulation, show_lidar_data
+from window import Window
 
-
-object = Object(100, 100, size=8)
-sim = Simulation('maps/map.png', num_measurements=500, std=0, object=object)
-window_name = "Lidar Simulation"
-
-
-while not is_pressed('q'):
-    cv2.imshow(window_name, sim.get_img())
-
-    if is_pressed('w'):
-        sim.move(0, 5, 0)
-    if is_pressed("s"):
-        sim.move(0, -5, 0)
-    if is_pressed('a'):
-        sim.move(-5, 0, 0)
-    if is_pressed('d'):
-        sim.move(5, 0, 0)
-
-    #rotate left
-    if is_pressed('r'):
-        sim.move(0, 0, 0.1)
-    #rotate right
-    if is_pressed('t'):
-        sim.move(0, 0, -0.1)
-
-    if is_pressed('g'):
-        lst = sim.get_lidar_data()
-        show_lidar_data(lst)
-
-
-    cv2.waitKey(10)
-
-
-
-cv2.destroyAllWindows()
-
-
+        
+if __name__ == '__main__':
+    window = Window()
+    window.run()
+        
 
 
 
--- simulation/helpers.py
+++ simulation/helpers.py
@@ -1,18 +1,19 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt 
+
+matplotlib.use('agg')
 
 
 def show_lidar_data(data: np.ndarray):
     x = np.cos(data[:, 0] + np.pi/2)*data[:, 1]
     y = np.sin(data[:, 0] + np.pi/2)*data[:, 1]
     
+    plt.figure()
     plt.scatter(x, y)
     plt.scatter([0], [0])
 
     plt.title("Lidar data visualization")
     plt.grid()
     plt.axis('scaled')
-    plt.show()
-
-
-
+    plt.savefig("data/lidar_data.png")
